2db6ccbba17f119cf164e2d62cf577c8dc78733c
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-01 23:31:21 +0000

    +user route

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true });
 
@@ -18,8 +17,10 @@ connection.once("open", () => {
   console.log("Databse connection established");
 });
 
+const usersRouter = require("./routes/users");
 
+app.use("/users", usersRouter);
 
-app.listen(port, ()=>{
-    console.log(`Server running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
