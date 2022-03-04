d27487ccf0b845677f0059c6ea0abb2b66d3bd89
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-04 10:04:39 +0000

    +add Comment route

--- routes/posts.js
+++ routes/posts.js
@@ -26,17 +26,28 @@ router.route("/add").post(auth, (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
-router.route("/add-comment/:id").post(auth,(req, res) => {
+router.route("/add-comment/:id").post(auth, (req, res) => {
   const { id } = req.params;
   const { content } = req.body;
-  const post = Post.findById(id);
-  console.log(post)
   const newContent = new Comment({
     content,
     likes: 0
   });
-  post.comments.push(newContent);
+  Post.findByIdAndUpdate(
+    { _id: id },
+    { $push: { comments: newContent } },
+    (err, data) => {
+      if (err) console.log("comment error", err);
+      else console.log("comment added", data);
+    }
+  );
   newContent.save().then(() => res.json("Comment Added"));
 });
+
+
+router.route("/comments/:id").get(auth, (req, res)=>{
+    
+})
+
 
 module.exports = router;
